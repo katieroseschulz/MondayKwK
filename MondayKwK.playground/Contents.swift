9177c1c8d59bfd3da1f0e1b48b39059a87abbc89
Apple <apple@Apples-MacBook-Pro-10.local>
2019-07-22 15:02:16 -0400

    initialized git

---
+++ MondayKwK.playground/Contents.swift
@@ -0,0 +1,78 @@
+/*
+
+/*
+ 
+ */
+
+import Cocoa
+
+var str = "Hello, playground" // note
+
+// string - a series of characters in between 2 quotation marks
+// integer - whole numbers, positive or negative
+// double
+// interpolation
+// variable
+// constant
+// keyword
+
+var address = "1313 Mockingbird Lane"
+address = "254 Ocean Ave"
+
+let name = "Katie" // Uses less memory and can make a machine run faster. Only good for permanent information.
+
+//Quiz
+var string = 5 // the data type is...integer
+
+var empty:Double // Data types are always written with a Capital. When you are defining a variable you use the equal sign, but when you are just saying what data type it is you use :
+
+var printMe = "hello world" // camel case
+print (printMe)
+
+var printK = "hi my name is katie"
+print (printK)
+
+var printS = "happy summer"
+print (printS)
+
+/*
+ sample
+ Katie
+ */
+
+var firstName =  "Katie"
+print ("Hi, my name is \(firstName).")
+
+var printM = "what is 5 + 5?"
+print (printM)
+print ("the answer is \(5+5)")
+
+var printA = "what is 9.7 + -3?"
+print (printA)
+print ("the answer is \(9.7-3)")
+
+var temp = 87
+
+if temp > 80 {
+    print ("then wear shorts")
+}
+if temp < 80 {
+    print ("then wear pants")
+}
+
+if temp > 80 {
+    print ("Wear shorts")
+} else {
+    print ("Wear Pants")
+}
+
+if temp == 87 {
+    print ("I guessed correct")
+} else {
+    print ("It is not 87")
+}
+
+ */
+
+get init
+
